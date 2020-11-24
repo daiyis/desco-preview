955f969856cba9c0e3796cb1c1faaad3f008bbda
Leo Dai <leo@jobadder.com>
2020-11-25 08:51:17 +1100

    add less loader

--- projects/leods/src/public-api.ts
+++ projects/leods/src/public-api.ts
@@ -5,4 +5,4 @@
 export * from './lib/button/button.component';
 export * from './lib/leods.component';
 export * from './lib/leods.module';
-// export * from './lib/nz-zorro.module';
+export * from './lib/nz-zorro.module';
